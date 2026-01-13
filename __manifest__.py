0e47ef74d3b526b8be274d01806ade894888545f
leenalsinji <leensinji234@gmail.com>
2026-01-13 09:47:07 +0200

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Real Estate',
+    'depends': ['base'],
+    'application': True,
+    'category': 'Services',
+'data': [
+    'security/ir.model.access.csv',
+    'views/estate_property_views.xml',
+    'views/estate_menus.xml',
+],
+
+
+
+
+
+}
